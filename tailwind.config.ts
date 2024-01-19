09f7bbf43b8d455dd85b8145ea286d28ad72eca0
Bishnu <thapabishnu20@gmail.com>
2024-01-19 18:45:07 +0545

    install material tailwind & configured

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,13 @@
 import type { Config } from "tailwindcss";
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+module.exports = withMT({
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+});
 
 const config: Config = {
   content: [
